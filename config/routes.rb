bfbe5af52aeeb2762f03d8b9a6b59af984d32385
d4mk0 <d4mk0f@gmail.com>
2016-05-05 21:56:55 +0300

    Сгенерировали контроллер Home с экшном About, для расположения информации о программе и добавили файл локализации для русского языка

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/about'
+
   devise_for :users
+  root to: 'home#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
