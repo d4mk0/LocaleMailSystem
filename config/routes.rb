cffb469ca548de7f7b8314f70b71ce499f2f50ad
d4mk0 <d4mk0f@gmail.com>
2016-05-10 13:47:32 +0300

    Правки в путях и в меню

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'home/about'
 
   devise_for :users
-  root to: 'home#about'
+  root to: 'messages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
